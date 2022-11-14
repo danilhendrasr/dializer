e69f6c4852dfa6718650eb1f45f3900b69493841
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-14 10:03:06 +0700

    fix: missing output arrow in output node

--- apps/frontend/src/app/components/output.node.tsx
+++ apps/frontend/src/app/components/output.node.tsx
@@ -21,28 +21,35 @@ const OutputNode: React.FC<Props> = (props) => {
       onMouseEnter={() => setDisplayAddNodeBtn(true)}
       onMouseLeave={() => setDisplayAddNodeBtn(false)}
     >
-      <Group x={x} y={y}>
-        <Shape
-          sceneFunc={(context, shape) => {
-            context.beginPath();
-            context.moveTo(20, 0);
-            context.lineTo(width + 20, 0);
-            context.lineTo(width, height);
-            context.lineTo(-20, height);
-            context.lineTo(0, 0);
-            context.closePath();
+      <Group>
+        <Group x={x} y={y}>
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(20, 0);
+              context.lineTo(width + 20, 0);
+              context.lineTo(width, height);
+              context.lineTo(-20, height);
+              context.lineTo(0, 0);
+              context.closePath();
 
-            context.fillStrokeShape(shape);
-          }}
-          stroke={isActive ? 'red' : 'grey'}
-        />
-        <Text
-          align="center"
-          width={width}
-          height={height}
-          verticalAlign="middle"
-          text={'This is output node'}
-        />
+              context.fillStrokeShape(shape);
+            }}
+            stroke={isActive ? 'red' : 'grey'}
+          />
+          <Text
+            align="center"
+            width={width}
+            height={height}
+            verticalAlign="middle"
+            text={'This is output node'}
+          />
+          <Arrow
+            points={[width, 0, width + 50, 0]}
+            strokeWidth={1}
+            stroke={isActive ? 'red' : 'grey'}
+          />
+        </Group>
         {next && displayAddNodeBtn ? (
           <AddNodeBtn
             isActive={displayAddNodeBtn}
