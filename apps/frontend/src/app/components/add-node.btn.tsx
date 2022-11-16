c6d3a98d2a9acd647be7b0e377f5e0af6adcca0f
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-17 05:11:05 +0700

    feat(frontend): add onclick prop to add node button

--- apps/frontend/src/app/components/add-node.btn.tsx
+++ apps/frontend/src/app/components/add-node.btn.tsx
@@ -9,11 +9,15 @@ type Props = {
 
   onMouseEnter: () => void;
   onMouseLeave: () => void;
+  onClick?: () => void;
 };
 
 export const AddNodeBtn: React.FC<Props> = (props) => {
-  const { isActive, x, y, onMouseEnter, onMouseLeave } = props;
+  const { isActive, x, y, onMouseEnter, onMouseLeave, onClick } = props;
   const [hovered, setHovered] = useState(false);
+
+  const radius = hovered ? 10 : 5;
+  const opacity = hovered ? 1 : 0.9;
 
   return (
     <Portal selector={'.top-layer'} enabled={isActive}>
@@ -26,12 +30,13 @@ export const AddNodeBtn: React.FC<Props> = (props) => {
           onMouseLeave();
           setHovered(false);
         }}
+        onClick={onClick}
       >
         <Circle
           x={x}
           y={y}
-          radius={hovered ? 10 : 5}
-          opacity={hovered ? 1 : 0.9}
+          radius={radius}
+          opacity={opacity}
           stroke="black"
           fill="white"
           strokeWidth={0.5}
