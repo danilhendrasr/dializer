1e87bba336f5fde291af4989de56d79c8a5a6af4
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-17 05:37:07 +0700

    refactor(frontend): move node conditional rendering logic to app.tsx

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,14 +2,23 @@ import React, { useEffect, useReducer, useRef, useState } from 'react';
 import { KonvaEventObject } from 'konva/lib/Node';
 import { Stage as StageClass } from 'konva/lib/Stage';
 import { Stage, Layer } from 'react-konva';
-import { nodeToKonvaNode as nodeObjectToKonvaNode } from './utils';
 import { Vector2d } from 'konva/lib/types';
 import { ToggleAnimationBtn } from './components/play-animation.btn';
 import { EnvironmentPanel } from './components/environtment-panel';
 import { INITIAL_NODES } from './data';
 import { NodesContext } from './contexts/nodes.context';
 import { nodesReducer } from './reducers/nodes.reducer';
-import { NodeActions } from './types';
+import {
+  ConditionalNodeNextNodes,
+  FlowChartNode,
+  NodeActions,
+  NodeTypes,
+} from './types';
+import { StartEndNode } from './components/start-end.node';
+import { ProcessNode } from './components/process.node';
+import { InputNode } from './components/input.node';
+import { OutputNode } from './components/output.node';
+import { IfNode } from './components/if.node';
 
 const SCALE_BY = 1.2;
 
@@ -91,18 +100,91 @@ const App = () => {
       >
         <Layer>
           {nodes.map((node, idx) => {
-            let nextNode: Parameters<typeof nodeObjectToKonvaNode>[1] =
+            let next: FlowChartNode | ConditionalNodeNextNodes | undefined =
               undefined;
             if (node.nextIdx) {
-              nextNode = nodes[node.nextIdx];
+              next = nodes[node.nextIdx];
             } else if (node.nextIdxIfTrue && node.nextIdxIfFalse) {
-              nextNode = {
+              next = {
                 true: nodes[node.nextIdxIfTrue],
                 false: nodes[node.nextIdxIfFalse],
               };
             }
 
-            return nodeObjectToKonvaNode(node, nextNode, idx);
+            const onAddNewNode = () => {
+              nodesDispatch({
+                type: NodeActions.ADD_NEW,
+                idx: idx + 1,
+                nodeType: NodeTypes.PROCESS,
+              });
+            };
+
+            switch (node.type) {
+              case NodeTypes.START:
+              case NodeTypes.END:
+                return (
+                  <StartEndNode
+                    key={idx}
+                    x={node.x}
+                    y={node.y}
+                    isActive={node.active}
+                    type={node.type}
+                    next={next as FlowChartNode}
+                    onAddNewNode={onAddNewNode}
+                  />
+                );
+
+              case NodeTypes.PROCESS:
+                return (
+                  <ProcessNode
+                    key={idx}
+                    x={node.x}
+                    y={node.y}
+                    isActive={node.active}
+                    next={next as FlowChartNode}
+                    onAddNewNode={onAddNewNode}
+                  />
+                );
+
+              case NodeTypes.INPUT:
+                return (
+                  <InputNode
+                    key={idx}
+                    x={node.x}
+                    y={node.y}
+                    isActive={node.active}
+                    next={next as FlowChartNode}
+                    onAddNewNode={onAddNewNode}
+                  />
+                );
+
+              case NodeTypes.OUTPUT:
+                return (
+                  <OutputNode
+                    key={idx}
+                    x={node.x}
+                    y={node.y}
+                    isActive={node.active}
+                    next={next as FlowChartNode}
+                    onAddNewNode={onAddNewNode}
+                  />
+                );
+
+              case NodeTypes.IF:
+                return (
+                  <IfNode
+                    key={idx}
+                    x={node.x}
+                    y={node.y}
+                    isActive={node.active}
+                    next={next as ConditionalNodeNextNodes}
+                    onAddNewNode={onAddNewNode}
+                  />
+                );
+
+              default:
+                return null;
+            }
           })}
         </Layer>
         <Layer name="top-layer" />
