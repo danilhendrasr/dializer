90eda170b328bd48ee2d77d8e95be4373b17835b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-19 16:23:41 +0700

    feat(frontend): add background to close button

--- apps/frontend/src/app/components/close.icon.tsx
+++ apps/frontend/src/app/components/close.icon.tsx
@@ -1,4 +1,4 @@
-import { Group, Line } from 'react-konva';
+import { Circle, Group, Line } from 'react-konva';
 
 type Props = {
   x: number;
@@ -6,14 +6,17 @@ type Props = {
   size: number;
   // Defaults to black
   color?: string;
+  // Defaults to lightgrey
+  bgColor?: string;
   onClick?: () => void;
 };
 
 export const CloseIcon: React.FC<Props> = (props) => {
-  const { x, y, size, color = 'black', onClick } = props;
+  const { x, y, size, color = 'black', bgColor = 'lightgrey', onClick } = props;
 
   return (
     <Group onClick={onClick}>
+      <Circle x={x + size / 2} y={y + size / 2} radius={size} fill={bgColor} />
       <Line points={[x, y, x + size, y + size]} stroke={color} />
       <Line points={[x + size, y, x, y + size]} stroke={color} />
     </Group>
