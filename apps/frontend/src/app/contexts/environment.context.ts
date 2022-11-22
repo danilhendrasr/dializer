3a8bdc3182e36af02be6dc490c7fc92fd17b88c4
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-22 17:07:35 +0700

    feat(frontend): environment context

---
+++ apps/frontend/src/app/contexts/environment.context.ts
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from 'react';
+
+type EnvironmentContextValue = {
+  environment: Record<string, number> | undefined;
+  setEnvironment: React.Dispatch<
+    React.SetStateAction<Record<string, number> | undefined>
+  >;
+};
+
+const EnvironmentContext = createContext<EnvironmentContextValue | null>(null);
+
+export const EnvironmentContextProvider = EnvironmentContext.Provider;
+
+export function useEnvironmentContext() {
+  const environment = useContext(EnvironmentContext);
+  if (environment === undefined) {
+    throw new Error('Cannot use context outside of a provider.');
+  }
+
+  return environment;
+}
