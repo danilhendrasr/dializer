0b9ef2820ed1785924404c2fd212cb0cef253114
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-23 05:35:50 +0700

    feat(frontend): add context menu to app state context

--- apps/frontend/src/app/contexts/app-state.context.ts
+++ apps/frontend/src/app/contexts/app-state.context.ts
@@ -1,11 +1,19 @@
 import React, { createContext, useContext } from 'react';
-import { Coordinate } from '../types';
+import { Coordinate, NodeTypes } from '../types';
 
 type AppStateContext = {
   newVarPopover: {
     position: Coordinate | undefined;
     setNewVarPopover: React.Dispatch<
       React.SetStateAction<(Coordinate & { callerIdx: number }) | undefined>
+    >;
+  };
+  contextMenu: {
+    position: Coordinate | undefined;
+    setContextMenu: React.Dispatch<
+      React.SetStateAction<
+        (Coordinate & { callerIdx: number; callerType: NodeTypes }) | undefined
+      >
     >;
   };
 };
