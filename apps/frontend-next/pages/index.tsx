278a0b629338bd81c0991fb17b46dbed79c74531
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-26 21:45:14 +0700

    feat(frontend-next): workspaces page

--- apps/frontend-next/pages/index.tsx
+++ apps/frontend-next/pages/index.tsx
@@ -1,17 +1,82 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import styled from 'styled-components';
+import { Plus as PlusIcon } from 'tabler-icons-react';
 
-const StyledPage = styled.main`
-  width: 50%;
-  height: 50%;
-  background-color: red;
+const StyledPage = styled.div`
+  position: absolute;
+  width: 600px;
+  height: 450px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: sans-serif;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  padding: 10px 20px;
+  width: 100%;
+  margin-bottom: 15px;
+`;
+
+const NewWorkspaceBtn = styled.button`
+  margin: 20px;
+  border-radius: 8px;
+  border: none;
+  padding: 10px;
+  transition: 0.2s all;
+  align-self: flex-end;
+  box-sizing: border-box;
+
+  &:hover {
+    background-color: lightgrey;
+    transition: 0.2s all;
+    cursor: pointer;
+  }
+`;
+
+const EmptyWorkspaceWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 10px;
+  box-sizing: border-box;
+`;
+
+const NoWorkspaceText = styled.p`
+  font-family: sans-serif;
+  color: grey;
+  font-size: 0.85rem;
+  letter-spacing: 0.5px;
 `;
 
 export function Index() {
   return (
     <StyledPage>
-      <div className="wrapper">
-        <h1>Hello world</h1>
-      </div>
+      <Head>
+        <title>Workspaces | Dializer</title>
+      </Head>
+      <TopContainer>
+        <h1>Workspaces</h1>
+        <NewWorkspaceBtn>
+          <PlusIcon size={18} />
+        </NewWorkspaceBtn>
+      </TopContainer>
+      <EmptyWorkspaceWrapper>
+        <Image
+          src="/no-data.svg"
+          alt="No data decorative image."
+          width={155}
+          height={155}
+          priority={true}
+          style={{ margin: 15 }}
+        />
+        <NoWorkspaceText>You have no workspace.</NoWorkspaceText>
+      </EmptyWorkspaceWrapper>
     </StyledPage>
   );
 }
