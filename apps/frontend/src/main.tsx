9dd02de1f2e8f696f7d8648f4d898ee9b7640b3f
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-07 10:09:31 +0700

    chore: delete frontend and frontend-e2e project

--- apps/frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM from 'react-dom/client';
-
-import App from './app/app';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(<App />);
