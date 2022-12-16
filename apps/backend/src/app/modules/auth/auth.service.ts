4a4b62e4f175923795f9cd0a9312a10a7b49c5fc
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-16 08:14:48 +0700

    chore(backend): add missing type in auth service

--- apps/backend/src/app/modules/auth/auth.service.ts
+++ apps/backend/src/app/modules/auth/auth.service.ts
@@ -30,8 +30,8 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+  async login(user: User) {
+    const payload = { username: user.username, sub: user.id };
 
     return {
       access_token: this.jwtService.sign(payload),
