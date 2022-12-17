447fe2b95396343083b01515cb3b3fe7d80b903b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-17 07:26:36 +0700

    feat(frontend-next): make control panel icons use pointer cursor on hover

--- apps/frontend-next/components/play-animation.btn.tsx
+++ apps/frontend-next/components/play-animation.btn.tsx
@@ -9,8 +9,8 @@ export const ToggleAnimationBtn: React.FC<Props> = (props) => {
   const { isAnimationRunning, onClick } = props;
 
   return isAnimationRunning ? (
-    <PlayerPause size={15} fill={'red'} onClick={onClick} />
+    <PlayerPause size={15} fill={'red'} onClick={onClick} cursor="pointer" />
   ) : (
-    <PlayerPlay size={15} fill={'#90EE90'} onClick={onClick} />
+    <PlayerPlay size={15} fill={'#90EE90'} onClick={onClick} cursor="pointer" />
   );
 };
