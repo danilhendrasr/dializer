7b443db4545c48d1a955ad8b1ce5b08535458634
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-17 07:10:47 +0700

    feat(frontend-next): display workspace title at workspace page

--- apps/frontend-next/pages/workspaces/[workspace-id].tsx
+++ apps/frontend-next/pages/workspaces/[workspace-id].tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { useRouteProtection } from '../../hooks/use-route-protection.hook';
 import { useRouter } from 'next/router';
 import { useNodesStore } from '../../contexts/nodes.context';
+import styled from 'styled-components';
+import { WorkspaceEntity } from '@dializer/types';
 
 const Workspace = dynamic(
   () =>
@@ -14,11 +16,24 @@ const Workspace = dynamic(
 export default function WorkspaceWrapper() {
   useRouteProtection();
   const router = useRouter();
+  const [workspace, setWorkspace] = useState<WorkspaceEntity>();
   const fetchNodes = useNodesStore((state) => state.fetch);
 
   useEffect(() => {
     if (!router.isReady) return;
-    fetchNodes(router.query['workspace-id'] as string);
+    const workspaceId = router.query['workspace-id'] as string;
+    fetchNodes(workspaceId);
+
+    const fetchWorkspaceDetail = async () => {
+      const response = await fetch(
+        `http://localhost:3333/api/workspaces/${workspaceId}`
+      );
+
+      const jsonResponse = await response.json();
+      setWorkspace(jsonResponse);
+    };
+
+    fetchWorkspaceDetail();
   }, [router, fetchNodes]);
 
   return (
@@ -26,7 +41,30 @@ export default function WorkspaceWrapper() {
       <Head>
         <title>Flow Chart Editor | Dializer</title>
       </Head>
+      <WorkspaceTitleContainer>
+        <h1>{workspace && workspace.title}</h1>
+      </WorkspaceTitleContainer>
       <Workspace />
     </>
   );
 }
+const WorkspaceTitleContainer = styled.div`
+  position: absolute;
+  display: flex;
+  top: 10px;
+  left: 20px;
+  z-index: 999;
+  width: fit-content;
+  padding: 0 12px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid lightgrey;
+  /* gap: 10px; */
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  h1 {
+    font-family: sans-serif;
+    font-size: 0.9rem;
+    letter-spacing: 1px;
+  }
+`;
