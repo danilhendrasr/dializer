b5dcb09f0794618b5a84e24b4dd9febd2d5a142e
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-02-24 08:19:44 +0700

    feat(frontend-next): custom color palette

--- apps/frontend-next/tailwind.config.js
+++ apps/frontend-next/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     'apps/frontend-next/components/**/*.tsx',
   ],
   theme: {
+    colors: {
+      primary1: '#ffffff',
+      primary2: '#e5e5e5',
+      secondary: '#fca311',
+      accent1: '#14213d',
+      accent2: '#000000',
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
