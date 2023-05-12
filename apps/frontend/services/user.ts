8594f78ed963ddd24604d92cae94e62feb539a20
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-05-12 13:34:40 +0000

    feat(frontend): user service

---
+++ apps/frontend/services/user.ts
@@ -0,0 +1,33 @@
+import { ApiErrorResponse, UserEntity } from '@dializer/types';
+import { ApiService } from './base';
+
+export class UserService extends ApiService {
+  private static instance: UserService;
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+
+    this.instance = new UserService();
+    return this.instance;
+  }
+
+  async update(id: string, data: Partial<UserEntity>): Promise<UserEntity> {
+    const res = await fetch(`${this.apiUrl}/users/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    const jsonResp = await res.json();
+    if (!res.ok) {
+      throw new Error((jsonResp as ApiErrorResponse).message);
+    }
+
+    return jsonResp as UserEntity;
+  }
+}
