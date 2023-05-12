f8ef6127cb5e04ad90d33b6ea16385c46eda35f7
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-05-12 00:45:39 +0000

    feat(frontend): sign up method in auth service

--- apps/frontend/service/auth.ts
+++ apps/frontend/service/auth.ts
@@ -13,7 +13,8 @@ export class AuthService extends ApiService {
     return this.instance;
   }
 
-  async signIn(email: string, password: string) {
+  // Returns the access token if successful
+  async signIn(email: string, password: string): Promise<string> {
     const response = await fetch(`${this.apiUrl}/auth/login`, {
       method: 'POST',
       headers: {
@@ -21,6 +22,32 @@ export class AuthService extends ApiService {
       },
       body: JSON.stringify({
         username: email,
+        password,
+      }),
+    });
+
+    const jsonResponse = await response.json();
+    if (!response.ok) {
+      throw new Error((jsonResponse as ApiErrorResponse).message);
+    }
+
+    return (jsonResponse as { access_token: string }).access_token;
+  }
+
+  // Returns the access token if successful
+  async signUp(
+    fullName: string,
+    email: string,
+    password: string
+  ): Promise<string> {
+    const response = await fetch(`${this.apiUrl}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fullName,
+        email,
         password,
       }),
     });
