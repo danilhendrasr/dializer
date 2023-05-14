422e9b5e6a59adf7cdbbafc4f3ec9eeb580a5038
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-05-14 00:30:12 +0000

    refactor(frontend): use react-hook-form in forget password page

--- apps/frontend/pages/forget-password.tsx
+++ apps/frontend/pages/forget-password.tsx
@@ -2,32 +2,40 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { AuthTitle } from '../components/auth-title';
 import { AuthForm } from '../components/auth-form';
-import { FormEvent, useState } from 'react';
 import { AuthInput } from '../components/auth-input';
 import { AuthSubmitBtn } from '../components/auth-submit';
 import { toast } from 'react-toastify';
 import { UserService } from '../services/user';
+import { useForm, Controller } from 'react-hook-form';
+
+type ForgetPasswordInputs = {
+  email: string;
+};
 
 export default function ForgetPasswordPage() {
   const router = useRouter();
 
-  const [email, setEmail] = useState((router.query['email'] as string) ?? '');
-  const [isSubmittingData, setIsSubmittingData] = useState(false);
+  const {
+    handleSubmit,
+    control,
+    formState: { isDirty, isValid, isSubmitting },
+  } = useForm<ForgetPasswordInputs>({
+    mode: 'onChange',
+    defaultValues: {
+      email: (router.query['email'] as string) ?? '',
+    },
+  });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsSubmittingData(true);
+  const submitResetEmailRequest = async (values: ForgetPasswordInputs) => {
+    const { email } = values;
 
     try {
       await UserService.getInstance().sendPasswordResetEmail(email);
       toast.success(`Email sent to ${email}`);
-      setEmail('');
     } catch (e) {
       const err = e as Error;
       toast.error(err.message);
     }
-
-    setIsSubmittingData(false);
   };
 
   return (
@@ -38,23 +46,23 @@ export default function ForgetPasswordPage() {
 
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3">
         <AuthTitle>Forget Password</AuthTitle>
-        <AuthForm onSubmit={handleSubmit}>
+        <AuthForm onSubmit={handleSubmit(submitResetEmailRequest)}>
           <p className="mb-4 text-sm">
             An email with a link to reset your password will be sent to the
             following email:
           </p>
-          <AuthInput
-            id="email"
+          <Controller
+            control={control}
             name="email"
-            placeholder="Email"
-            value={email}
-            onChangeHandler={(e) => setEmail(e.target.value)}
-            disabled={isSubmittingData}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <AuthInput {...field} type="email" placeholder="Email" />
+            )}
           />
           <AuthSubmitBtn
-            isSubmitting={isSubmittingData}
-            disabled={!email || isSubmittingData}
-            text={isSubmittingData ? 'Sending Email...' : 'Send Email'}
+            isSubmitting={isSubmitting}
+            disabled={!isDirty || !isValid || isSubmitting}
+            text={isSubmitting ? 'Sending Email...' : 'Send Email'}
           />
         </AuthForm>
       </div>
