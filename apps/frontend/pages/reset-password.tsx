a92a12d56964f36789e31157ebd73317bb0e603f
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-05-14 00:39:34 +0000

    refactor(frontend): use react-hook-form in reset password page

--- apps/frontend/pages/reset-password.tsx
+++ apps/frontend/pages/reset-password.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthTitle } from '../components/auth-title';
 import { AuthForm } from '../components/auth-form';
 import { AuthInput } from '../components/auth-input';
@@ -8,14 +8,25 @@ import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import * as base64 from 'base-64';
 import { UserService } from '../services/user';
+import { useForm, Controller } from 'react-hook-form';
 
 type TokenPayload = { id: string; email: string } | null;
 
+type ResetPasswordInputs = {
+  newPassword: string;
+  confirmPassword: string;
+};
+
 export default function ResetPasswordPage() {
   const router = useRouter();
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [isSubmittingData, setIsSubmittingData] = useState(false);
+
+  const {
+    handleSubmit,
+    watch,
+    control,
+    formState: { isDirty, isValid, isSubmitting, errors },
+  } = useForm<ResetPasswordInputs>({ mode: 'onChange' });
+
   const [payload, setPayload] = useState<TokenPayload>(null);
 
   useEffect(() => {
@@ -30,17 +41,10 @@ export default function ResetPasswordPage() {
     setPayload(tokenPayload);
   }, [router.isReady]);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (newPassword !== confirmPassword) {
-      toast('Password and confirmed password does not match.', {
-        type: 'error',
-      });
-      return;
-    }
+  const resetPassword = async (values: ResetPasswordInputs) => {
+    const { newPassword } = values;
 
     try {
-      setIsSubmittingData(true);
       await UserService.getInstance().resetPassword(
         router.query['token'] as string,
         newPassword
@@ -55,8 +59,6 @@ export default function ResetPasswordPage() {
     } catch (e) {
       const err = e as Error;
       toast.error(err.message);
-    } finally {
-      setIsSubmittingData(false);
     }
   };
 
@@ -68,30 +70,44 @@ export default function ResetPasswordPage() {
 
       <main className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/3">
         <AuthTitle>Reset Password</AuthTitle>
-        <AuthForm onSubmit={handleSubmit}>
+        <AuthForm onSubmit={handleSubmit(resetPassword)}>
           <p className="mb-4 text-sm">Reset password for: {payload?.email}.</p>
-          <AuthInput
-            id="password"
-            name="password"
-            placeholder="New Password"
-            type="password"
-            value={newPassword}
-            onChangeHandler={(e) => setNewPassword(e.target.value)}
-            disabled={isSubmittingData}
+          <Controller
+            control={control}
+            name="newPassword"
+            rules={{ required: true }}
+            render={({ field }) => (
+              <AuthInput
+                {...field}
+                type="password"
+                placeholder="New Password"
+              />
+            )}
           />
-          <AuthInput
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChangeHandler={(e) => setConfirmPassword(e.target.value)}
-            disabled={isSubmittingData}
+          <Controller
+            control={control}
+            name="confirmPassword"
+            rules={{
+              required: true,
+              validate: (value) => {
+                if (value !== watch('newPassword')) {
+                  return 'Password do not match.';
+                }
+              },
+            }}
+            render={({ field }) => (
+              <AuthInput
+                {...field}
+                type="password"
+                placeholder="Confirm Password"
+                bottomLabel={errors.confirmPassword?.message}
+              />
+            )}
           />
           <AuthSubmitBtn
-            isSubmitting={isSubmittingData}
-            disabled={!newPassword || !confirmPassword || isSubmittingData}
-            text={isSubmittingData ? 'Resetting Password' : 'Reset Password'}
+            isSubmitting={isSubmitting}
+            disabled={!isDirty || !isValid || isSubmitting}
+            text={isSubmitting ? 'Resetting Password' : 'Reset Password'}
           />
         </AuthForm>
       </main>
