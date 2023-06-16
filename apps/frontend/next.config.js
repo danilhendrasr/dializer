fe8be0ec53d36ede9da6c4cae89cc8d7b5e03c45
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2023-06-16 20:18:30 +0700

    fix(frontend): _next/static not found when serving in production

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,7 +2,6 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nx/next/plugins/with-nx');
-const path = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -12,10 +11,6 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
-  },
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
   },
   async redirects() {
     return [
